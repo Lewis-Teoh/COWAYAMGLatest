7b0860243fed8f3d3e445fbde8348170454e7f20
Tan Chun Shui <chunshui7347@gmail.com>
2018-12-02 12:19:00 +0800

    #update

--- app/lucy/lucy.js
+++ app/lucy/lucy.js
@@ -9,6 +9,39 @@ angular.module('webApp.lucy', ['ngRoute'])
 		});
 	}])
 
-	.controller('LucyCtrl', ['$scope', , function ($scope) {
+	.controller('LucyCtrl', ['$scope', '$firebaseArray' , function ($scope, $firebaseArray) {
+        var ref = firebase.database().ref().child('Orders');
+		$scope.orders = $firebaseArray(ref);
+		
+
+		$scope.createOrder = function () {
+			var name = $scope.orders.nameTxt;
+			var email = $scope.orders.emailTxt;
+			var phoneNo = $scope.orders.phoneNoTxt;
+			var productCode = $scope.orders.productCodeTxt;
+			var address = $scope.orders.addressTxt;
+			var message = $scope.orders.messageTxt;
+			$scope.orders.$add({
+				name: name,
+				email: email,
+				phoneNo: phoneNo,
+				address: address,
+				productCode: productCode,
+				message: message
+			}).then(function (ref) {
+				console.log(ref);
+				$scope.success = true;
+				window.setTimeout(function () {
+					$scope.$apply(function () {
+						$scope.success = false;
+					});
+				}, 2000);
+				
+			}, function (error) {
+				console.log(error);
+			});
+			
+		};
 
     }]);
+
